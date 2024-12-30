e7813e3d0e654974228cf63ff1c3895fcfa2b921
Rijwan1267 <mdrijwan1267@gmail.com>
2024-12-30 19:35:25 +0530

    Add new files

---
+++ style.js
@@ -0,0 +1,18 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+
+
+
+btnEl.addEventListener("click", () =>{
+    // console.log(addNewImages);
+    addNewImages()
+});
+
+function addNewImages() {
+    const newImgEl = document.createElement("img");
+    
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`;
+
+    imageContainerEl.appendChild(newImgEl);
+}
